94721e300501eaaacb0f1dc972466e38daafd884
lige1123 <zhangze_8_8@sina.com>
2020-02-28 12:54:04 +0800

    add b.c

---
+++ b.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: b.c
+	> Author: zhangze
+	> Mail: zhangze_8_8sina.com
+	> Created Time: Fri 28 Feb 2020 12:52:19 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main() {
+    int a, b;
+    return 0;
+}
+
