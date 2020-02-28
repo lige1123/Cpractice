a60534bab19ad98184330e3f275cef9b32257d38
lige1123 <zhangze_8_8@sina.com>
2020-02-28 12:35:05 +0800

    add a file named helloworld.c

---
+++ helloworld.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: helloworld.c
+	> Author: zhangze
+	> Mail: zhangze_8_8sina.com
+	> Created Time: Fri 28 Feb 2020 12:33:41 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main() {
+    printf("Hello World!\n");
+    return 0;
+}
+
+
